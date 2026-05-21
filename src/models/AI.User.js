d0d52459ca6820fcc01306ed37d4c2426dea34cc
catherine1265 <catherine.sharonn@gmail.com>
2026-05-21 11:30:21 +0700

    Update AI.User.js

--- src/models/AI.User.js
+++ src/models/AI.User.js
@@ -1,1 +1,50 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/AI.database');
+const bcrypt = require('bcryptjs');
+
+const User = sequelize.define('User', {
+  user_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  phone_number: {
+    type: DataTypes.STRING(20)
+  },
+  role: {
+    type: DataTypes.ENUM('donor', 'volunteer', 'community_rep', 'admin'),
+    defaultValue: 'donor'
+  },
+  is_active: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  }
+}, {
+  tableName: 'users',
+  timestamps: true,
+  hooks: {
+    beforeCreate: async (user) => {
+      user.password = await bcrypt.hash(user.password, 10);
+    }
+  }
+});
+
+User.prototype.comparePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = User;
 
