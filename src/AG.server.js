d07f568de8e8a8837574aea4a16207c2141765c3
catherine1265 <catherine.sharonn@gmail.com>
2026-05-21 11:27:33 +0700

    Update AG.server.js

--- src/AG.server.js
+++ src/AG.server.js
@@ -1,1 +1,21 @@
-
+require('dotenv').config();
+const app = require('./app');
+const sequelize = require('./config/database');
+const PORT = process.env.PORT || 5000;
+// Database sync
+sequelize.authenticate()
+.then(() => {
+console.log('✓ Database connected');
+return sequelize.sync({ alter: true });
+})
+.then(() => {
+console.log('✓ Models synced');
+app.listen(PORT, () => {
+console.log(`Server running on http://localhost:${PORT}`);
+});
+})
+.catch(err => {
+console.error('Database error:', err);
+process.exit(1);
+});
+module.exports = { sequelize };
